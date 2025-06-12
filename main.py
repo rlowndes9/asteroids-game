c8285b2fcb752a44a93f9ea8edc8f0836b44adc7
rlowndes9 <rlowndes9@gmail.com>
2025-06-12 16:05:37 +0100

    Created player sprite and other changes

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock =pygame.time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     
-    while True:
+    while True: # Main Game Loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
     
 if __name__ == "__main__":
     main()
